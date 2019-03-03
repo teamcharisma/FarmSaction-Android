cb5795865b786cf5f23d1bab0a5c8a2d4f88ffce
Shyam M <m.shyam.tnj@gmail.com>
2019-03-03 16:08:38 +0530

    Fix data

--- app/src/main/java/io/github/teamcharisma/farmsaction/SelectCategoryActivity.java
+++ app/src/main/java/io/github/teamcharisma/farmsaction/SelectCategoryActivity.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 public class SelectCategoryActivity extends AppCompatActivity {
     Intent oldintent;
     Bundle extras;
-    ArrayList<Boolean> categories;
+    String category;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,11 +34,6 @@ public class SelectCategoryActivity extends AppCompatActivity {
         LinearLayout button_customwork = findViewById(R.id.icon_customwork);
         LinearLayout button_crop = findViewById(R.id.icon_crop);
 
-        categories = new ArrayList<>();
-        for (int i = 0; i < 11; i++)
-            categories.add(false);
-
-
 
         View.OnClickListener onclick = new View.OnClickListener(){
             @Override
@@ -46,91 +41,91 @@ public class SelectCategoryActivity extends AppCompatActivity {
                 Log.d("shyam","touched");
                 switch (v.getId()){
                     case R.id.icon_electricity:
-                        categories.set(0, !categories.get(0));
-                        if (categories.get(0))
-                            button_electricity.setBackground(getResources().getDrawable(R.drawable.card_background));
-                        else
-                            button_electricity.setBackground(getResources().getDrawable(R.drawable.card_background_dark));
+                        category = "electricity";
+                        // (categories.get(0))
+                           // button_electricity.setBackground(getResources().getDrawable(R.drawable.card_background));
+                      //  else
+                           // button_electricity.setBackground(getResources().getDrawable(R.drawable.card_background_dark));
                         break;
 
                     case R.id.icon_fertilizer:
-                        categories.set(1, !categories.get(1));
-                        if (categories.get(1))
-                            button_fertilizer.setBackground(getResources().getDrawable(R.drawable.card_background));
-                        else
-                            button_fertilizer.setBackground(getResources().getDrawable(R.drawable.card_background_dark));
+                        category = "fertilizers";
+                        // (categories.get(0))
+                        // button_electricity.setBackground(getResources().getDrawable(R.drawable.card_background));
+                        //  else
+                        // button_electricity.setBackground(getResources().getDrawable(R.drawable.card_background_dark));
                         break;
 
                     case R.id.icon_fuel:
-                        categories.set(2, !categories.get(2));
-                        if (categories.get(2))
-                            button_fuel.setBackground(getResources().getDrawable(R.drawable.card_background));
-                        else
-                            button_fuel.setBackground(getResources().getDrawable(R.drawable.card_background_dark));
+                        category = "fuel";
+                        // (categories.get(0))
+                        // button_electricity.setBackground(getResources().getDrawable(R.drawable.card_background));
+                        //  else
+                        // button_electricity.setBackground(getResources().getDrawable(R.drawable.card_background_dark));
                         break;
 
                     case R.id.icon_interest:
-                        categories.set(3, !categories.get(3));
-                        if (categories.get(3))
-                            button_interest.setBackground(getResources().getDrawable(R.drawable.card_background));
-                        else
-                            button_interest.setBackground(getResources().getDrawable(R.drawable.card_background_dark));
+                        category = "interest";
+                        // (categories.get(0))
+                        // button_electricity.setBackground(getResources().getDrawable(R.drawable.card_background));
+                        //  else
+                        // button_electricity.setBackground(getResources().getDrawable(R.drawable.card_background_dark));\
                         break;
 
                     case R.id.icon_labour:
-                        categories.set(4, !categories.get(4));
-                        if (categories.get(4))
-                            button_labour.setBackground(getResources().getDrawable(R.drawable.card_background));
-                        else
-                            button_labour.setBackground(getResources().getDrawable(R.drawable.card_background_dark));
+                        category = "labour";
+                        // (categories.get(0))
+                        // button_electricity.setBackground(getResources().getDrawable(R.drawable.card_background));
+                        //  else
+                        // button_electricity.setBackground(getResources().getDrawable(R.drawable.card_background_dark));
                         break;
 
                     case R.id.icon_livestock:
-                        categories.set(5, !categories.get(5));
-                        if (categories.get(5))
-                            button_livestock.setBackground(getResources().getDrawable(R.drawable.card_background));
-                        else
-                            button_livestock.setBackground(getResources().getDrawable(R.drawable.card_background_dark));
+                        category = "livestock";
+                        // (categories.get(0))
+                        // button_electricity.setBackground(getResources().getDrawable(R.drawable.card_background));
+                        //  else
+                        // button_electricity.setBackground(getResources().getDrawable(R.drawable.card_background_dark));
                         break;
 
                     case R.id.icon_machinery:
-                        categories.set(6, !categories.get(6));
-                        if (categories.get(6))
-                            button_machinery.setBackground(getResources().getDrawable(R.drawable.card_background));
-                        else
-                            button_machinery.setBackground(getResources().getDrawable(R.drawable.card_background_dark));
+                        category = "machinery";
+                        // (categories.get(0))
+                        // button_electricity.setBackground(getResources().getDrawable(R.drawable.card_background));
+                        //  else
+                        // button_electricity.setBackground(getResources().getDrawable(R.drawable.card_background_dark));
                         break;
 
                     case R.id.icon_pesticides:
-                        categories.set(7, !categories.get(7));
-                        if (categories.get(7))
-                            button_pesticides.setBackground(getResources().getDrawable(R.drawable.card_background));
-                        else
-                            button_pesticides.setBackground(getResources().getDrawable(R.drawable.card_background_dark));
+                        category = "pesticides";
+                        // (categories.get(0))
+                        // button_electricity.setBackground(getResources().getDrawable(R.drawable.card_background));
+                        //  else
+                        // button_electricity.setBackground(getResources().getDrawable(R.drawable.card_background_dark));
                         break;
 
                     case R.id.icon_rent:
-                        categories.set(8, !categories.get(8));
-                        if (categories.get(8))
-                            button_rent.setBackground(getResources().getDrawable(R.drawable.card_background));
-                        else
-                            button_rent.setBackground(getResources().getDrawable(R.drawable.card_background_dark));
+                        category = "rent";
+                        // (categories.get(0))
+                        // button_electricity.setBackground(getResources().getDrawable(R.drawable.card_background));
+                        //  else
+                        // button_electricity.setBackground(getResources().getDrawable(R.drawable.card_background_dark));
                         break;
 
                     case R.id.icon_customwork:
-                        categories.set(9, !categories.get(9));
-                        if (categories.get(9))
-                            button_customwork.setBackground(getResources().getDrawable(R.drawable.card_background));
-                        else
-                            button_customwork.setBackground(getResources().getDrawable(R.drawable.card_background_dark));
+                        category = "customwork";
+                        // (categories.get(0))
+                        // button_electricity.setBackground(getResources().getDrawable(R.drawable.card_background));
+                        //  else
+                        // button_electricity.setBackground(getResources().getDrawable(R.drawable.card_background_dark));
                         break;
 
                     case R.id.icon_crop:
-                        categories.set(10, !categories.get(10));
-                        if (categories.get(10))
-                            button_crop.setBackground(getResources().getDrawable(R.drawable.card_background));
-                        else
-                            button_crop.setBackground(getResources().getDrawable(R.drawable.card_background_dark));
+                        category = "crop";
+                        // (categories.get(0))
+                        // button_electricity.setBackground(getResources().getDrawable(R.drawable.card_background));
+                        //  else
+                        // button_electricity.setBackground(getResources().getDrawable(R.drawable.card_background_dark));
                         break;
                 }
 
@@ -149,14 +144,18 @@ public class SelectCategoryActivity extends AppCompatActivity {
         button_customwork.setOnClickListener(onclick);
         button_crop.setOnClickListener(onclick);
 
-        Log.d("shyam", extras.getStringArrayList("itemnames").toString());
-
     }
 
     public void onclick (View w) {
         Intent intent = new Intent(getBaseContext(), AddCropActivity.class);
         intent.putExtra("itemnames", extras.getStringArrayList("itemnames"));
         intent.putExtra("itemprices", extras.getStringArrayList("itemprices"));
+        ArrayList<String> categories;
+        if (oldintent.hasExtra("categories"))
+            categories = extras.getStringArrayList("categories");
+        else
+            categories = new ArrayList<>();
+        categories.add(category);
         intent.putExtra("categories", categories);
         startActivity(intent);
     }
